afba8dc48316f6ef7106619dfc009d0a316e6df3
paul-frdev <pavelzadyrko@gmail.com>
2021-03-17 18:09:16 +0200

    added ideas

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,12 @@
 $(function () {
-  
+  $('.header-top__lang-link').on('click', function () {
+    $('.header-top__lang-link').toggleClass('header-top__lang-link--active');
+  });
 });
+
+
+
+
+
+
 
